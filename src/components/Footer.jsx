fb5d4a2ec9270c578452b99dad2d274384f4c19d
Mohsin Khan <mkhan6720@gmail.com>
2024-01-30 19:26:59 +0530

    MVP ready

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,28 @@ import styles from "./Footer.module.css";
 function Footer() {
   return (
     <div className={styles.footer}>
-      BlockScan &copy; {new Date().getFullYear()}
+      <div>
+        Made by{" "}
+        <a
+          className="customLink"
+          href="https://twitter.com/khan_mohsin07"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Mohsin Khan
+        </a>
+      </div>
+      <div>
+        Check out the{" "}
+        <a
+          className="customLink"
+          href="https://github.com/mohsin-khan07/chain-surge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Source Code
+        </a>
+      </div>
     </div>
   );
 }
