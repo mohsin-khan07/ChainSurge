d80c6300d2f3f1b622c3569fad999e72f2b545c8
Mohsin Khan <mkhan6720@gmail.com>
2024-02-11 16:19:49 +0530

    ui completed

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -4,9 +4,9 @@ import BlocksCard from "../components/blocks/BlocksCard";
 
 function Homepage() {
   return (
-    <div className="py-6 sm:py-10 px-5 xxs:px-8 md:px-14 lg:px-24">
+    <div className="px-5 py-6 xxs:px-8 sm:py-10 md:px-14 lg:px-24">
       <OverviewCard />
-      <div className="flex flex-col lg:flex-row gap-8">
+      <div className="flex flex-col gap-8 lg:flex-row">
         <BlocksCard />
         <TransactionsCard />
       </div>
