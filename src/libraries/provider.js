09f01b0a28c00424e2e85003f21d40a96d1d4d37
Mohsin Khan <mkhan6720@gmail.com>
2024-02-12 13:04:36 +0530

    back to normal - using import.meta

--- src/libraries/provider.js
+++ src/libraries/provider.js
@@ -1,7 +1,7 @@
 import { Alchemy, Network } from "alchemy-sdk";
 
 const settings = {
-  apiKey: process.env.ALCHEMY_API,
+  apiKey: import.meta.env.VITE_ALCHEMY_API,
   network: Network.ETH_MAINNET,
 };
 
