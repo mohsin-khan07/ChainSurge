dee978d367815f767b1c83c6afb2bee74bccb0e9
Mohsin Khan <mkhan6720@gmail.com>
2024-02-12 12:54:48 +0530

    testing env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
+dotenv.config();
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      replace: {
+        "process.env.ALCHEMY_API": JSON.stringify(process.env.ALCHEMY_API),
+      },
+    },
+  },
+});
